c1bb5ff52dd2cb1c794f1c3ae1f3749e3f13c2d4
shivam123425 <shivam123425@gmail.com>
2018-05-12 23:46:28 +0530

    Add server.js, shifted img to css folder

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/css'));
+
+app.get("/", (req,res)=> {
+    res.sendFile(__dirname+"/index.html");
+});
+
+
+
+
+app.listen(port, ()=> {
+    console.log("Started the server");
+});
